493f1fd1124d98784ef00931ba69f6bee02fe0c5
Lekan Ojulowo <lekanojulowo@gmail.com>
2020-04-11 12:43:16 +0100

    fix issues with chrome lighthouse report

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 "use strict";
+
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker.register('service-worker.js')
+}
+
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('/sw.js').then(function (registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//     }, function (err) {
+//       // registration failed :(
+//       console.log('ServiceWorker registration failed: ', err);
+//     });
+//   });
+// }
+
 const $q = document.querySelector.bind(document);
 const $qa = document.querySelectorAll.bind(document);
 
